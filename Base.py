a735978af4899000986073f8af2896f1f08e0e33
A <1069308752@qq.com>
2025-08-23 11:20:50 +0800

    优化代码，并整合文档切割，新增微信机器人功能。

--- Base.py
+++ Base.py
@@ -1,10 +1,19 @@
 from elasticsearch import Elasticsearch
 from LLMs import  LLMs
+import Prompt
+import json
+from docx import Document
+from tqdm import tqdm
+
+from wxauto import WeChat
+from wxauto.msgs import FriendMessage
+import time
 class Base_RAG():
-    def __init__(self, data_path: str = "http://localhost:9200", database: str = 'elasticsearch',base_url:str = 'https://open.bigmodel.cn/api/paas/v4/', api_key:str = '', model:str = 'glm-4-flash-250414'):
+    def __init__(self, data_path: str = "http://localhost:9200", database: str = 'elasticsearch',base_url:str = 'https://open.bigmodel.cn/api/paas/v4/', api_key:str = '', model:str = 'glm-4-flash-250414', _async:bool = True):
         self.data_path = data_path
         self.database = database 
-        self.llm = LLMs(api_key= api_key,base_url = base_url, model = model)
+        self._async = _async
+        self.llm = LLMs(api_key= api_key,base_url = base_url, model = model, _async = _async)
         if self.database not in ['elasticsearch']:
             raise TypeError("数据库类型仅支持[elasticsearch]")
         if self.database == 'elasticsearch':
@@ -12,7 +21,7 @@ class Base_RAG():
         
     #检测是否能够连接数据库
     def connection_elasticsearch(self):
-        if self.cilent.ping():
+        if self.client.ping():
             print( "✓ ping测试成功")
             return True
         else:
@@ -23,11 +32,15 @@ class Base_RAG():
     def chunk_insert(self, index:str,data:dict):
         self.client.index(index = index, document=data)
 
-    #进行对话
-    async def chat(self, question: str, docs: list):
-        respose = await self.llm.chat(question = question, Type='rag', docs = docs)
-        async for item in respose:          
-            print(item)
+    #进行对话(异步模式)
+    async def Asycchat(self, question: str, qtype:str = 'normal', prompt:str = "", arg_dict:dict = {}, stream:str=True):
+        respose = await self.llm.Asycchat(question = question, qtype=qtype, prompt = prompt,stream = stream, arg_dict = arg_dict)
+        await respose
+
+    #进行对话(同步模式)
+    def chat(self, question: str, qtype:str = 'normal', prompt:str = "", arg_dict:dict = {}, stream:str=True):
+        respose = self.llm.chat(question = question, qtype=qtype, prompt = prompt,stream = stream, arg_dict = arg_dict)
+        return respose
 
     #获取有哪些知识库
     def get_index(self):
@@ -49,8 +62,65 @@ class Base_RAG():
                 ids.append(response['hits']['hits'][i]['_id'])
         return ids, docs
 
+    # 提供文档切割的接口
+    def file_split(self, filepath:str):
+        if filepath.endswith('.docx'):
+            file = Document(filepath)
+            text = []
+            chunk = []
+            for element in tqdm(file.element.body):
+                if element.tag.endswith('p'):
+                    # 
+                    arg = {
+                        'documnet':''.join(text),
+                        'sentence':element.text
+                    }
+                    res = self.chat(qtype = 'custom', prompt=Prompt.TITLE_IDENTIFICATION, question=Prompt.TITLE_IDENTIFICATION_USER,arg_dict = arg,stream = False)
+                    if json.loads(res.to_json())['choices'][0]['message']['content'] == '正文':
+                        text.append(element.text+'\n')
+                    else:
+                        chunk.append(''.join(text[:-1]))
+                        text = []
 
-# a = Base_RAG(api_key="71f9c7ff218f8f7329a95c794a42c149.SfLL8ycOqirgYmvd")
+                if element.tag.endswith('tbl'):
+                    table_c = 0
+                    for row in element.tr_lst:
+                        for tc in row.tc_lst:
+                            for p in tc.p_lst:
+                                if table_c == 0:
+                                    text.append(p.text+' | ')
+                        text.append('\n')
+            return chunk
+        elif filepath.endswith('.pdf'):
+            pass
+        else:
+            raise ValueError("文件只支持.docx、.pdf文件")
+
+    def connect_wx(self, groupname:str = "",nickname:str = ""):
+        def on_message(msg, chat):
+            if isinstance(msg, FriendMessage):
+                time.sleep(2)
+                if "@" + nickname == msg.content[0:len(nickname)+1]:
+                    answer = self.chat(qtype='custom',stream=False, prompt=Prompt.RAG_PROMPT, question=Prompt.RAG_USER, arg_dict={
+    'chunks':"".join(a.chunk_search(index="os_qa_pizza", question=msg.content[1+len(nickname):])[1]),
+    'question':msg.content[1+len(nickname):]
+})
+                    msg.quote(json.loads(answer.to_json())['choices'][0]['message']['content'])
+        
+        wx = WeChat()
+
+        wx.AddListenChat(nickname=groupname, callback=on_message)
+
+        wx.KeepRunning()
+
+        
+
+a = Base_RAG(api_key="71f9c7ff218f8f7329a95c794a42c149.SfLL8ycOqirgYmvd", _async=False)
+
+# chunk = a.file_split(filepath = "./FILE/oo.docx")
+a.connect_wx("robot_test","test_robot")
+
+
 # import asyncio
 # docs = a.chunk_search(index = 'os_qa_pizza', question ="什么是操作系统")[1]
 # asyncio.run(a.chat("什么是操作系统",docs))
--- LLMs.py
+++ LLMs.py
@@ -1,39 +1,36 @@
-from openai import AsyncOpenAI
+from openai import AsyncOpenAI, OpenAI
 
 
 class LLMs():
-    def __init__(self, api_key:str = '', base_url:str = '', docs:list = [], model:str=''):
+    def __init__(self, api_key:str = '', base_url:str = '', model:str='', _async:bool = True):
         self.api_key = api_key
-
+        self._async = _async
         if self.api_key == '':
             raise TypeError("api_key 不可以为空")
         
         #异步调取模型服务端
-        self.client = AsyncOpenAI(
-            api_key= self.api_key,
-            base_url=base_url
-        )
-        print(self.api_key, base_url)
+        if self._async:
+            self.client = AsyncOpenAI(
+                api_key= self.api_key,
+                base_url=base_url
+            )
+        else:
+            self.client = OpenAI(
+                api_key= self.api_key,
+                base_url=base_url
+            )
+        
         if model == '':
             raise TypeError("模型名称不可为空")
         self.model = model
         #此处需要增加异常捕捉措施
 
 
-    async def chat(self, question:str, Type:str, docs:list = []):
+    async def Asycchat(self, question:str, qtype:str, prompt:str = "" , arg_dict:dict = {},stream:bool=True):
+        if self._async == False:
+            raise ValueError("不可以在非异步模式下使用该函数")
         try:
-            if Type == 'rag':
-                if len(docs) == 0:
-                    raise ValueError("文档不可为空")
-                response = await self.client.chat.completions.create(
-                    model= self.model,
-                    messages=[
-                        {"role": "system", "content": f"用户想了解{question}。请先提炼问题的核心意思，然后结合以下知识库中的内容，给出最清晰、最准确的答案，如果没有问题的准确答案就回答:找不到准确答案，尽量减少其他多余回答"},
-                        {"role":"user", "content":f"文档为{'\n'.join(docs)}"}
-                    ],
-                    stream=True
-            )
-            elif Type == 'normal':
+            if qtype == 'normal':
                 response = await self.client.chat.completions.create(
                     model= self.model,
                     messages=[
@@ -41,8 +38,41 @@ class LLMs():
                     ],
                     stream=True
             )
+            elif qtype == 'custom':
+                response = await self.client.chat.completions.create(
+                    model= self.model,
+                    messages=[
+                        {"role":"system", "content":f"{prompt.format_map(arg_dict)}"},
+                        {"role":"user", "content":f"{question.format_map(arg_dict)}"}
+                    ],
+                    stream=stream
+            )
         except Exception as e:
             raise TypeError(f"{e}")
         
         return response
     
+    def chat(self, question:str, qtype:str, prompt:str = "" , arg_dict:dict = {},stream:bool=True):
+        if self._async:
+            raise ValueError("不可以在异步模式下使用该函数")
+        try:
+            if qtype == 'normal':
+                response = self.client.chat.completions.create(
+                    model= self.model,
+                    messages=[
+                        {"role":"user", "content":f"{question}"}
+                    ],
+                    stream=stream
+            )
+            elif qtype == 'custom':
+                response = self.client.chat.completions.create(
+                    model= self.model,
+                    messages=[
+                        {"role":"system", "content":f"{prompt.format_map(arg_dict)}"},
+                        {"role":"user", "content":f"{question.format_map(arg_dict)}"}
+                    ],
+                    stream=stream
+            )
+        except Exception as e:
+            raise TypeError(f"{e}")
+        return response
